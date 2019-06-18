1b36baf61445c79a44f075e3276aa8e5af43df8a
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2019-06-19 06:43:26 +0700

    Kurang

--- projects/ng2-smart-table/src/lib/components/filter/filter.module.ts
+++ projects/ng2-smart-table/src/lib/components/filter/filter.module.ts
@@ -19,6 +19,7 @@ const FILTER_COMPONENTS = [
   CustomFilterComponent,
   CheckboxFilterComponent,
   CompleterFilterComponent,
+  DateFilterComponent,
   InputFilterComponent,
   SelectFilterComponent,
 ];
